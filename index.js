aaad3f32c7d818818ceab1cd08fdc46e4af8f3ce
Manish <bhagat.manishV5@gmail.com>
2025-08-19 16:27:32 +0530

    uibuild

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const Phonebook = require('./models/Phonebook')
 
 
 
@@ -42,7 +44,13 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/persons', (request, response) => {
-    response.send(phonebook)
+    // const res = Phonebook
+    // .find({})
+    // response.send(res)
+    Phonebook
+        .find({})
+        .then(persons => response.json(persons))
+        .catch(error => console.log(error))
 })
 
 app.get('/info', (request, response) => {
@@ -50,27 +58,39 @@ app.get('/info', (request, response) => {
     response.send(`Phonebook has info for ${phonebook.length} people` + '<br/><br/>' + dateTimestamp.toString())
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    const person = phonebook.find(p => p.id === id)
+app.get('/api/persons/:id', (request, response, next) => {
+    // const id = request.params.id
+    // const person = phonebook.find(p => p.id === id)
 
-    if (person) {
-        response.send(person)
-    } else {
-        response.status(404).end()
-    }
+    // if (person) 
+    //     response.send(person)
+    // } else {
+    //     response.status(404).end()
+    // }
+    Phonebook
+        .findById(request.params.id)
+        .then(result => {
+            response.send(result)
+        })
+        .catch(error => next(error))
  })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    const newPhonebook = phonebook.filter((p) => p.id !== id)
-    phonebook = newPhonebook
+app.delete('/api/persons/:id', (request, response, next) => {
+    // const id = request.params.id
+    // const newPhonebook = phonebook.filter((p) => p.id !== id)
+    // phonebook = newPhonebook
 
-    response.status(204).end()
+    // response.status(204).end()
+    Phonebook
+        .findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const person = request.body
 
     if (!person.name) {
@@ -81,19 +101,34 @@ app.post('/api/persons', (request, response) => {
         response.status(400).send({error: 'must include number'}).end()
     }
 
-    const existingPerson = phonebook.filter(p => p.name === person.name)
-    if (existingPerson.length > 0) {
-        response.status(400).send({error: 'name must be unique'}).end()
-        return
-    }
+    // const existingPerson = phonebook.filter(p => p.name === person.name)
+    // if (existingPerson.length > 0) {
+    //     response.status(400).send({error: 'name must be unique'}).end()
+    //     return
+    // }
 
-    const id = getRandomInt()
-    const newPerson = { ...person, id: String(id)}
-    phonebook.push(newPerson)
-    response.send(newPerson)
+    // const id = getRandomInt()
+    const newPerson = new Phonebook({ ...person })
+    newPerson
+        .save()
+        .then(() => response.send(person))
+        .catch(error => next(error))
 })
 
-const PORT = 3001
+const errorHandler = (error, request, response, next) => {
+    console.log(error)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }   else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+    }
+    next(error)
+}
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
